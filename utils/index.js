834911c5ed166a1e2a33ba73ecbc7ca137ccebf8
Daryl Carr <darylcarr@live.co.uk>
2017-05-30 09:09:46 +0100

    Remove reference to getBattleResult

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     getArchEnemy: require('./getArchEnemy'),
-    getBattleResult: require('./getBattleResult'),
     getFile: require('./getFile'),
     getFileMany: require('./getFileMany'),
     getFileNoRes: require('./getFileNoRes'),
