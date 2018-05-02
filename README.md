e5f61cf43aed946ad3a2848e22d5e2dc51c43bc9
MitchLloyd <mitchelllloyd154@gmail.com>
2018-05-02 15:52:45 +0100

    remove async library problems

--- README.md
+++ README.md
@@ -1,55 +1,61 @@
-#Understanding Asynchronous Code
+# Understanding Asynchronous Code
+
 Not all Javascript code happens in the order it's written - it is often **asynchronous**, as Javascript will continue to execute subsequent functions if previous calls need to wait for a response. We have many tools we can utilise to handle this, and this sprint focuses on **callbacks**.
 
 Callbacks are a common way that programmers can work with results from asynchronous functions, and underlie all other methods of handling asynchronous code. A callback is a function passed elsewhere to be invoked, allowing data to be passed back to the original function. During this sprint we will focus on the core concepts of async code. You will make calls to other functions that have already been written for you - it's your job to tie the functions together to return the response we're looking for.
 
 ## Objectives
 
-1. To understand the mechanisms and terminology of asynchronous code.
-2. Using TDD to ensure we're writing quality code.
-3. Study the documentation for and put into practise features of the async.js library.
+1.  To understand the mechanisms and terminology of asynchronous code.
+2.  Using TDD to ensure we're writing quality code.
 
 ## Functions Available To You
+
 Don't worry about the utils folder. Pretend that these functions are third-party APIs that you are requesting data from and that will respond after a short period of time.
 
 These are the functions available to you and a sample response from each one:
-    getSuperHeroes(your callback here) //=>  [returns an array of superhero names]
-    getArchEnemy(hero, yourCallbackHere) //=> 'villain name'
-    getFile(filename, yourCallbackHere) //=> 'File contents of filename'
-    getFileMany(filename, yourCallbackHere) //=> Your callback will be called multiple times
-    getPizzaById(id, yourCallbackHere) //=> {id: 1, name: 'Margarita', ingredients: ['cheese', 'tomato']}
-    getLocalPizzaShops(yourCallbackHere) //=> [{id: 1, name: 'Adrians Pizza Place'}]
-    getPizzasForShopId(shopId, yourCallbackHere) //=> [{id: 1, name: 'Margarita', ingredients: ['cheese', 'tomato']}]
+
+```js
+getSuperHeroes(your callback here) //=> [returns an array of superhero names]
+
+getArchEnemy(hero, yourCallbackHere); //=> 'villain name'
+
+getFile(filename, yourCallbackHere); //=> 'File contents of filename'
+
+getFileMany(filename, yourCallbackHere); //=> Your callback will be called multiple times
+```
 
 ## Stage 1
 
-1. Write a function `blockingEcho` that takes a string. It then waits three seconds from now, using a while loop, and then returns the string.
+1.  Write a function `blockingEcho` that takes a string. It then waits three seconds from now, using a while loop, and then returns the string.
 
 This is called 'blocking code' as it will not let Javascript run any other code while it waits. Call it three times in a row to see how long it takes. Clearly, this is not optimal.
 
-2. Asychronous code allows Javascript to continue running whilst waiting on results. Rewrite the previous function, call it `asyncEcho` and use setTimeout. It should take a string and an error-first callback as its arguments. This is now non-blocking code - Javascript will deal with other things and then, when the timer ends, it will deal with the callback. Call this function three times to see how long it takes.
+2.  Asychronous code allows Javascript to continue running whilst waiting on results. Rewrite the previous function, call it `asyncEcho` and use setTimeout. It should take a string and an error-first callback as its arguments. This is now non-blocking code - Javascript will deal with other things and then, when the timer ends, it will deal with the callback. Call this function three times to see how long it takes.
 
-3. Write a function called `fetchSuperHeroes`. It will need to take an error-first callback as its only argument. This function will need to invoke our `getSuperHeroes` function and, once it has received a response, it will capitalise each superhero name. Finally, invoke the callback with an array of capitalised superhero names.
+3.  Write a function called `fetchSuperHeroes`. It will need to take an error-first callback as its only argument. This function will need to invoke our `getSuperHeroes` function and, once it has received a response, it will capitalise each superhero name. Finally, invoke the callback with an array of capitalised superhero names.
 
-4. Write a function called `fetchOpponents` that takes an error-first callback function, and will use the `fetchSuperHeroes` function you just wrote to get an array of capitalised superheroes. It then needs to invoke our function `getArchEnemy` with each of the superhero names. As you start receiving results from `getArchEnemy`, build up an array of objects with hero and villain properties. Once you have had all the responses from `getArchEnemy`, invoke your callback with your array of objects sorted alphabetically by superhero name. Example:
-```[{hero: 'BATMAN', villain: 'THE JOKER'}, {hero: 'CAPTAIN AMERICA', villain: 'RED SKULL'}, ...]```
-The array will take some time to build with the results of multiple calls to the villain database. **How will you know when it is ready to invoke the callback?**
+4.  Write a function called `fetchOpponents` that takes an error-first callback function, and will use the `fetchSuperHeroes` function you just wrote to get an array of capitalised superheroes. It then needs to invoke our function `getArchEnemy` with each of the superhero names. As you start receiving results from `getArchEnemy`, build up an array of objects with hero and villain properties. Once you have had all the responses from `getArchEnemy`, invoke your callback with your array of objects sorted alphabetically by superhero name. Example:
+    `[{hero: 'BATMAN', villain: 'THE JOKER'}, {hero: 'CAPTAIN AMERICA', villain: 'RED SKULL'}, ...]`
+    The array will take some time to build with the results of multiple calls to the villain database. **How will you know when it is ready to invoke the callback?**
 
 ## Stage 2
+
 This section considers some of the issues that callbacks and asynchronous code may face.
 
-5. Write a function called `fetchContentOfFiles` that takes an array of file names and an error-first callback. It will need to invoke `getFile(fileName, yourCallbackFunction)` for each file in the array. It then needs to collect all the responses and, once it has received the final response, it will need to invoke the callback in the order they were requested - not in the order they were returned in.**How will you keep track of the order of responses?**
+5.  Write a function called `fetchContentOfFiles` that takes an array of file names and an error-first callback. It will need to invoke `getFile(fileName, yourCallbackFunction)` for each file in the array. It then needs to collect all the responses and, once it has received the final response, it will need to invoke the callback in the order they were requested - not in the order they were returned in.**How will you keep track of the order of responses?**
 
-6. Write a function called `fetchFilesAndLog`. It takes an array of file names and an error-first callback. It needs to invoke `getFile(fileName, yourCallbackFunction)` for each file in the array. It must log the results in the order they were requested and as soon as possible. If we have files 1, 2, 3 and 4, and 1 comes back first, we can log 1. If 3 then comes back, we need to wait until 2 has come back, and then log 2 and 3, and then 4 when we have the response. Once all the responses have been received, it needs to invoke the callback with the string 'Complete!'
+6.  Write a function called `fetchFilesAndLog`. It takes an array of file names and an error-first callback. It needs to invoke `getFile(fileName, yourCallbackFunction)` for each file in the array. It must log the results in the order they were requested and as soon as possible. If we have files 1, 2, 3 and 4, and 1 comes back first, we can log 1. If 3 then comes back, we need to wait until 2 has come back, and then log 2 and 3, and then 4 when we have the response. Once all the responses have been received, it needs to invoke the callback with the string 'Complete!'
 
 This is a useful technique in terms of user design - it can make loading times seem faster when the user gets the information they have requested sooner than if they had had to wait for the whole collection of responses.
 
-7. Multiple callbacks can be problematic - a credit card validation, for example, risks taking payment multiple times. Write a function called `fetchFileWithSingleCall` that takes a filename and an error-first callback. This function will need to invoke `getFileMany(fileName, yourCallbackFunction)`. You need to ensure this callback is only called once. You have written a lowbar function that may help with this.
+7.  Multiple callbacks can be problematic - a credit card validation, for example, risks taking payment multiple times. Write a function called `fetchFileWithSingleCall` that takes a filename and an error-first callback. This function will need to invoke `getFileMany(fileName, yourCallbackFunction)`. You need to ensure this callback is only called once. You have written a lowbar function that may help with this.
 
-##Stage 2.5 - an optional detour
+## Stage 3
 
 Build a project generator. The goal is to be able to use the terminal command `$generate my_new_project` and it should create a directory with the project's name in your current location, then fill it with the necessary files to start a basic JS project.
 These should include:
+
 * an index.js
 * a spec folder
 * an index.spec.js
@@ -58,25 +64,13 @@ These should include:
 * an eslint config file
 * a .gitignore file
 * a git repo initialised
-In order to do this you will need to utilise the **File System** module available in Node. Only use the asynchronous methods - any ending in 'sync' are not allowed.
+  In order to do this you will need to utilise the **File System** module available in Node. Only use the asynchronous methods - any ending in 'sync' are not allowed.
 
 A big part of problem solving is reading documentation and establishing what you will need and how to use it - here is the [documention for Node's File System module](https://nodejs.org/api/fs.html). You'll also have to research how to install your program on your computer to make the generate command globally available from your terminal.
 
 **Advanced Features**
 
 * Configure your generator to automatically run npm install when used, installing all the packages listed in the package.json.
-* If called like so: generate [project name] [github http], it should automatically add the github http as a remote.
+* If called like so: generate [project name][github http], it should automatically add the github http as a remote.
 * It should perform an inital commit with the message 'initial commit' and push it to your github on the link provided.
 * Explore how you could add interactivity purely with Node, i.e. ask the user to input the name of the project on the console or select between a choice of project templates.
-
-##Stage 3
-
-In this section we'll look at the [async.js library](https://caolan.github.io/async/). Similar to underscore or lodash, it has each, map and filter methods that act as asynchronous versions of their sync counterparts. Methods with 'series' in their name will wait until they get a response from one async function before it calls the next one. 'Limit' versions of the methods will limit the number of async functions running at any one time. The 'waterfall' method, which will execute a series of functions, passing data down from function to function, may also be useful.
-
-8. Write a function called `fetchPizzas` that takes an array of pizza IDs (use [1, 2, 3, 4]) and an error-first callback. Using async.js on the array of IDs, invoke `getPizzaById` for each ID. Once you have all the pizza responses, invoke the callback with an array of pizza objects.
-
-9. Rewrite tasks 6 and 7 using async.js.
-
-10. Write a function called `fetchLocalPizzaShopInfo` which takes an error-first callback as its argument. Use async.js to invoke `getLocalPizzaShops` which will return an array of pizzaShop objects. Iterate through these and filter out the ones that do not deliver. For the remaning shops, invoke `getPizzasForShopId`, passing in each shop ID. This will invoke its callback with an array of pizzas. Add these pizzas as a `pizzas` property to the relevant pizza shop object. Once all the pizza info has been received, invoke the error-first callback with the array of pizza shop objects.
-
-Consider async.waterfall for this task!
--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,263 +1,161 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 const sinon = require('sinon');
 const {
-    blockingEcho,
-    asyncEcho,
-    fetchSuperHeroes,
-    fetchOpponents,
-    fetchFilesAndLog,
-    fetchContentOfFiles,
-    fetchFileWithSingleCall,
-    fetchPizzas,
-    fetchLocalPizzaShopInfo
+  blockingEcho,
+  asyncEcho,
+  fetchSuperHeroes,
+  fetchOpponents,
+  fetchFilesAndLog,
+  fetchContentOfFiles,
+  fetchFileWithSingleCall
 } = require('../main');
 
-
-describe('blockingEcho', function () {
-    it('should be a function', function () {
-        expect(blockingEcho).to.be.a('function');
-    });
-    it('should return input', function () {
-        this.timeout(5000);
-        expect(blockingEcho('woo')).to.equal('woo');
-    });
+describe('blockingEcho', function() {
+  it('should be a function', function() {
+    expect(blockingEcho).to.be.a('function');
+  });
+  it('should return input', function() {
+    this.timeout(5000);
+    expect(blockingEcho('woo')).to.equal('woo');
+  });
 });
 
-
-describe('asyncEcho', function () {
-    it('should be a function', function () {
-        expect(asyncEcho).to.be.a('function');
-    });
-    it('should return input', function (done) {
-        this.timeout(5000);
-        const str = 'woo';
-        const cb = function (err, res) {
-            expect(res).to.equal(str);
-            done();
-        };
-        asyncEcho(str, cb);
-    });
+describe('asyncEcho', function() {
+  it('should be a function', function() {
+    expect(asyncEcho).to.be.a('function');
+  });
+  it('should return input', function(done) {
+    this.timeout(5000);
+    const str = 'woo';
+    const cb = function(err, res) {
+      expect(res).to.equal(str);
+      done();
+    };
+    asyncEcho(str, cb);
+  });
 });
 
-
-describe('fetchSuperHeroes', function () {
-    it('should be a function', function () {
-        expect(fetchSuperHeroes).to.be.a('function');
+describe('fetchSuperHeroes', function() {
+  it('should be a function', function() {
+    expect(fetchSuperHeroes).to.be.a('function');
+  });
+  it('should invoke the callback with no error', function(done) {
+    fetchSuperHeroes(function(err) {
+      expect(err).to.be.null;
+      done();
     });
-    it('should invoke the callback with no error', function (done) {
-        fetchSuperHeroes(function (err) {
-            expect(err).to.be.null;
-            done();
-        });
+  });
+  it('should invoke the callback with an array of 7 heroes', function(done) {
+    fetchSuperHeroes(function(err, heroes) {
+      expect(heroes).to.be.an('array');
+      expect(heroes.length).to.equal(7);
+      done();
     });
-    it('should invoke the callback with an array of 7 heroes', function (done) {
-        fetchSuperHeroes(function (err, heroes) {
-            expect(heroes).to.be.an('array');
-            expect(heroes.length).to.equal(7);
-            done();
-        });
+  });
+  it('should invoke the callback with an array of capitalised heroes', function(done) {
+    fetchSuperHeroes(function(err, heroes) {
+      expect(heroes[0]).to.equal(heroes[0].toUpperCase());
+      done();
     });
-    it('should invoke the callback with an array of capitalised heroes', function (done) {
-        fetchSuperHeroes(function (err, heroes) {
-            expect(heroes[0]).to.equal(heroes[0].toUpperCase());
-            done();
-        });
-    });
+  });
 });
-
 
 describe('fetchOpponents', () => {
-    it('should be a function', function () {
-        expect(fetchOpponents).to.be.a('function');
+  it('should be a function', function() {
+    expect(fetchOpponents).to.be.a('function');
+  });
+  it('should return an array', function(done) {
+    fetchOpponents(function(err, pairs) {
+      expect(pairs).to.be.an('array');
+      done();
     });
-    it('should return an array', function (done) {
-        fetchOpponents(function (err, pairs) {
-            expect(pairs).to.be.an('array');
-            done();
-        });
-    }).timeout(4000);
-    it('should return array of hero villain objects', function (done) {
-        fetchOpponents(function (err, pairs) {
-            expect(pairs[0].hasOwnProperty('hero')).to.equal(true);
-            expect(pairs[0].hasOwnProperty('villain')).to.equal(true);
-            done();
-        });
-    }).timeout(4000);
-    it('should return array of objects sorted alphabetically by hero names', function (done) {
-        fetchOpponents(function (err, pairs) {
-            expect(pairs).to.eql([
-            { hero: 'BATMAN', villain: 'THE JOKER' },
-            { hero: 'CAPTAIN AMERICA', villain: 'RED SKULL' },
-            { hero: 'SPIDERMAN', villain: 'GREEN GOBLIN' },
-            { hero: 'SUPERMAN', villain: 'LEX LUTHOR' },
-            { hero: 'THE FLASH', villain: 'ZOOM' },
-            { hero: 'THOR', villain: 'LOKI' },
-            { hero: 'WOLVERINE', villain: 'SABRETOOTH' }]
-          );
-            done();
-        });
-    }).timeout(4000);
+  }).timeout(4000);
+  it('should return array of hero villain objects', function(done) {
+    fetchOpponents(function(err, pairs) {
+      expect(pairs[0].hasOwnProperty('hero')).to.equal(true);
+      expect(pairs[0].hasOwnProperty('villain')).to.equal(true);
+      done();
+    });
+  }).timeout(4000);
+  it('should return array of objects sorted alphabetically by hero names', function(done) {
+    fetchOpponents(function(err, pairs) {
+      expect(pairs).to.eql([
+        { hero: 'BATMAN', villain: 'THE JOKER' },
+        { hero: 'CAPTAIN AMERICA', villain: 'RED SKULL' },
+        { hero: 'SPIDERMAN', villain: 'GREEN GOBLIN' },
+        { hero: 'SUPERMAN', villain: 'LEX LUTHOR' },
+        { hero: 'THE FLASH', villain: 'ZOOM' },
+        { hero: 'THOR', villain: 'LOKI' },
+        { hero: 'WOLVERINE', villain: 'SABRETOOTH' }
+      ]);
+      done();
+    });
+  }).timeout(4000);
 });
 
-
-describe('fetchContentOfFiles', function () {
-    it('exists', function () {
-        expect(fetchContentOfFiles).to.be.a('function');
-    });
-    it('should return a response for each file', function (done) {
-        let fileNames = [1, 2, 3, 4, 5];
-        function cb(err, files) {
-            expect(files.length).to.equal(fileNames.length);
-            done();
-        }
-        fetchContentOfFiles(fileNames, cb);
-    });
-    it('should invoke callback with filenames in order', function (done) {
-
-        let fileNames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        function cb(err, files) {
-            expect(files.length).to.equal(fileNames.length);
-            fileNames.forEach(function (name, i) {
-                expect(files[i]).to.equal(`File contents of ${name}`);
-            });
-            done();
-        }
-        fetchContentOfFiles(fileNames, cb);
-    });
+describe('fetchContentOfFiles', function() {
+  it('exists', function() {
+    expect(fetchContentOfFiles).to.be.a('function');
+  });
+  it('should return a response for each file', function(done) {
+    let fileNames = [1, 2, 3, 4, 5];
+    function cb(err, files) {
+      expect(files.length).to.equal(fileNames.length);
+      done();
+    }
+    fetchContentOfFiles(fileNames, cb);
+  });
+  it('should invoke callback with filenames in order', function(done) {
+    let fileNames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    function cb(err, files) {
+      expect(files.length).to.equal(fileNames.length);
+      fileNames.forEach(function(name, i) {
+        expect(files[i]).to.equal(`File contents of ${name}`);
+      });
+      done();
+    }
+    fetchContentOfFiles(fileNames, cb);
+  });
 });
 
-
-describe('fetchFilesAndLog', function () {
-    it('should be a function', function () {        
-        expect(fetchFilesAndLog).to.be.a('function');
+describe('fetchFilesAndLog', function() {
+  it('should be a function', function() {
+    expect(fetchFilesAndLog).to.be.a('function');
+  });
+  it('should log each file', function(done) {
+    let spy = sinon.spy(console, 'log');
+    const fileNames = [1, 2, 3, 4, 5, 6, 7];
+    fetchFilesAndLog(fileNames, function() {
+      expect(spy.callCount).to.equal(7);
+      fileNames.forEach(fileName => {
+        expect(spy.calledWith(`File contents of ${fileName}`)).to.be.true;
+      });
+      done();
     });
-    it('should log each file', function (done) {
-        let spy = sinon.spy(console,'log');
-        const fileNames = [1, 2, 3, 4, 5, 6, 7];
-        fetchFilesAndLog(fileNames, function () {
-            expect(spy.callCount).to.equal(7);
-            fileNames.forEach((fileName) => {
-                expect(spy.calledWith(`File contents of ${fileName}`)).to.be.true;
-            });
-            done();
-        });
-    }).timeout(6000);
+  }).timeout(6000);
 });
 
-
-describe('fetchFileWithSingleCall', function () {
-    it('exists', function () {
-        expect(fetchFileWithSingleCall).to.be.a('function');
-    });
-    it('responds with file name', function () {
-        const fileName = 'TOP SECRET';
-        function cb(err, file) {
-            expect(file).to.equal(`File contents of ${fileName}`);
-        }
-        fetchFileWithSingleCall(fileName, cb);
-    });
-    it('The call back should only be called once', function (done) {
-        const fileName = 'DO NOT OPEN';
-        let counter = 0;
-        function cb() {
-            counter++;
-        }
-        fetchFileWithSingleCall(fileName, cb);
-        setTimeout(function () {
-            expect(counter).to.equal(1);
-            done();
-        }, 2000);
-    }).timeout(3000);
-});
-
-
-describe('fetchPizzas', function () {
-    it('exists', function () {
-        expect(fetchPizzas).to.be.a('function');
-    });
-    it('responds with an array of objects', function (done) {
-        const pizzaIds = [1, 2, 3, 4];
-        function cb(err, pizzas) {
-            pizzas.forEach(function (pizza) {
-                expect(pizza).to.be.an('object');
-            });
-            done();
-        }
-        fetchPizzas(pizzaIds, cb);
-    });
-    it('responds with an array of the correct pizza objects ', function (done) {
-        const pizzaIds = [1, 2, 3, 4];
-        function cb(err, pizzas) {
-            expect(pizzas[0]).to.eql({ id: 1, name: 'Margarita', ingredients: ['cheese', 'tomato'] });
-            done();
-        }
-        fetchPizzas(pizzaIds, cb);
-    });
-});
-
-
-describe('fetchLocalPizzaShopInfo', function () {
-    it('exists', function () {
-        expect(fetchLocalPizzaShopInfo).to.be.a('function');
-    });
-    it('should return an array of objects', function (done) {
-        function cb(err, pizzaShops) {
-            pizzaShops.forEach(function (shop) {
-                expect(shop).to.be.an('object');
-            });
-            done();
-        }
-        fetchLocalPizzaShopInfo(cb);
-    }).timeout(7000);
-    it('Should filter out those shops which don\'t deliver', function (done) {
-        function cb(err, pizzaShops) {
-            expect(pizzaShops.length).to.equal(4);
-            done();
-        }
-        fetchLocalPizzaShopInfo(cb);
-    }).timeout(7000);
-    it('should respond with the correct list of shops', function (done) {
-        const pizzaShops = [
-            { id: 1, name: 'Anat\'s Amazing Pizza', deliver: true },
-            { id: 3, name: 'Mitch\'s Marvelous Pizza', deliver: true },
-            { id: 4, name: 'Haz\'s Hungry Pizza', deliver: true },
-            { id: 5, name: 'Sam\'s Special Pizza', deliver: true }
-        ];
-
-        function cb(err, pizzaShops) {
-            expect(pizzaShops[0].pizzas).to.eql([
-                {
-                  "id": 1,
-                  "name": "Margarita",
-                  "ingredients": [
-                    "cheese",
-                    "tomato"
-                  ]
-                },
-                {
-                  "id": 2,
-                  "name": "Meat Feast",
-                  "ingredients": [
-                    "cheese",
-                    "tomato",
-                    "Ham",
-                    "Bacon",
-                    "Beef"
-                  ]
-                },
-                {
-                  "id": 4,
-                  "name": "Hawaian",
-                  "ingredients": [
-                    "cheese",
-                    "tomato",
-                    "Ham",
-                    "Pineapple"
-                  ]
-                }
-              ]);
-            done();
-        }
-        fetchLocalPizzaShopInfo(cb);
-    }).timeout(7000);
+describe('fetchFileWithSingleCall', function() {
+  it('exists', function() {
+    expect(fetchFileWithSingleCall).to.be.a('function');
+  });
+  it('responds with file name', function() {
+    const fileName = 'TOP SECRET';
+    function cb(err, file) {
+      expect(file).to.equal(`File contents of ${fileName}`);
+    }
+    fetchFileWithSingleCall(fileName, cb);
+  });
+  it('The call back should only be called once', function(done) {
+    const fileName = 'DO NOT OPEN';
+    let counter = 0;
+    function cb() {
+      counter++;
+    }
+    fetchFileWithSingleCall(fileName, cb);
+    setTimeout(function() {
+      expect(counter).to.equal(1);
+      done();
+    }, 2000);
+  }).timeout(3000);
 });
