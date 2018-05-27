a9a071c1263693e120098a62712ac15b28bf3f8f
ricksmith26 <ricksmith26@hotmail.com>
2018-05-27 12:38:06 +0100

    revision

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const {
   getFileMany,
   getSuperHeroes
 } = require('./utils/index');
+
+function blockingEcho(str) {
+  let endTime = Date.now() + 3000;
+  while (endTime > Date.now()) return str;
+}
+
+module.exports = blockingEcho;
